63dd6fbc02f71c3ae3e5725bf0ce52db8bbac967
Krishan Kant Agnihotri <60218702+KRISHANKANTAGNIHOTRI@users.noreply.github.com>
2021-05-14 10:05:13 +0530

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -1,1 +1,22 @@
+//Simple Explaination : Find the second minimum element in the array.
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+ ll n;
+ cin>>n;
+ vector<int> v(n);
+ for(int i = 0 ; i<n ;i++){
+    cin>>v[i];
+ }
+ ll mn = *min_element(v.begin(),v.end());
+ for(int i = 0 ;i<n ;i++){
+    if(v[i]!=mn){
+        cout<<v[i]<<"\n";
+        return 0;
+    }
+ }
+cout<<"NO\n";
 
+}
