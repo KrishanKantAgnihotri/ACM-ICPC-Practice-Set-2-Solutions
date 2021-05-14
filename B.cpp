fa6f25faed5ceef7997b382e5c48c8716b8c38b5
Krishan Kant Agnihotri <60218702+KRISHANKANTAGNIHOTRI@users.noreply.github.com>
2021-05-14 10:12:02 +0530

    Create B.cpp

---
+++ B.cpp
@@ -0,0 +1,23 @@
+
+Simple Explanation:  Brute force all pairs having difference <= d       
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+
+ ll n,d;
+ cin>>n>>d;
+ vector<int> v(n);
+ for(int i = 0 ; i<n ;i++)
+    cin>>v[i];
+ ll cnt = 0 ; 
+ for(int i =0 ; i<n; i++){
+    for(int j = 0 ;j<n ;j++){
+        if( i== j) continue;
+        if(abs(v[i]-v[j])<=d) cnt++;
+    }
+ }
+ cout<<cnt<<endl;
+}
